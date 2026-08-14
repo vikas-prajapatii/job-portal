8488cc08ae7ddb103914f21c337570e5d35af122
vikas-prajapatii <vikasprajapati@example.com>
2026-08-14 09:49:26 +0530

    fix: remove invalid EnableEurekaServer annotation from API Gateway Application class

--- cloud/job-portal-api-gateway/src/main/java/com/noir/job/JobPortalApiGatewayApplication.java
+++ cloud/job-portal-api-gateway/src/main/java/com/noir/job/JobPortalApiGatewayApplication.java
@@ -2,7 +2,7 @@ package com.noir.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableEurekaServer
+
 @SpringBootApplication
 public class JobPortalApiGatewayApplication {
 
