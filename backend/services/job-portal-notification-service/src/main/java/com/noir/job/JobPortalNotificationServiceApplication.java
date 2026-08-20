f7059b0ea2d69bb0f0222d5e1797d04262347655
unknown <vikasprajapati2327@gmail.comgit config --global user.name>
2026-08-20 18:43:53 +0530

    feat: add notification service structure, gateway routing, and central configurations

---
+++ backend/services/job-portal-notification-service/src/main/java/com/noir/job/JobPortalNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.noir.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalNotificationServiceApplication.class, args);
+	}
+
+}
