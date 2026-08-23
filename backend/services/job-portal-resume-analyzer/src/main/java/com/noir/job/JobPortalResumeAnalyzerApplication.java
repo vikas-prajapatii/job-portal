01d1b3699d6da6e8612034cefd5d0b1d0ad35732
vikas-prajapatii <vikasprajapati2327@gmail.com>
2026-08-23 20:56:22 +0530

    feat(resume-analyzer): add resume-analyzer module and config server setup

---
+++ backend/services/job-portal-resume-analyzer/src/main/java/com/noir/job/JobPortalResumeAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.noir.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalResumeAnalyzerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalResumeAnalyzerApplication.class, args);
+	}
+
+}
